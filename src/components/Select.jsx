9df057de850b94350f0f81dabb9d347909cf8cac
= <=>
2024-07-10 21:17:31 +0500

    Updated Select Component Beta Deployment

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,22 +1,21 @@
-import React , {useId} from 'react'
+import React, { forwardRef } from 'react';
+import { useId } from 'react-id-generator'; // Assuming useId is imported from a library like react-id-generator
 
-const Select = ({
-    options,
-    label,
-    className = "",
-    ...props
-},ref) => {
-    const id = useId();
-  return (
-    <div className='w-full '>
-      {label && <label htmlFor={id} className=''></label>}
-      <select {...props} id={id} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border-gray-200 w-full ${className}`}>
-       {options.map((item)=>(
-        
-       ))}
-      </select>
-    </div>
-  )
-}
+const Select = forwardRef(({ options, label, className = '', ...props }, ref) => {
+    const id = useId(); // Assuming useId generates a unique ID for accessibility
 
-export default Select
+    return (
+        <div className='w-full'>
+            {label && <label htmlFor={id} className=''></label>}
+            <select {...props} id={id} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border-gray-200 w-full ${className}`}>
+                {options.map((item) => (
+                    <option key={item} value={item}>
+                        {item}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+});
+
+export default Select;
